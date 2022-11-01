ebdabc8aa496d51ffec39098aa39aa38a67fd72a
Wojciech Wirkijowski <wojciech@wirkijowski.pl>
2022-11-01 10:52:20 +0100

    6.2: Custom Handler Function

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -2,11 +2,17 @@
 package web
 
 import (
+	"context"
+	"net/http"
 	"os"
 	"syscall"
 
 	"github.com/dimfeld/httptreemux/v5"
 )
+
+// A Handler is a type that handles an http request within our own little mini
+// framework.
+type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) error
 
 // App is the entrypoint into our application and what configures our context
 // object for each of our http handlers. Feel free to add any configuration
@@ -16,7 +22,7 @@ type App struct {
 	shutdown chan os.Signal
 }
 
-// Factory fucntion
+// Factory function
 // NewApp creates an App value that handle a set of routes for the application.
 func NewApp(shutdown chan os.Signal) *App {
 	return &App{
@@ -29,4 +35,27 @@ func NewApp(shutdown chan os.Signal) *App {
 // issue is identified.
 func (a *App) SignalShutdown() {
 	a.shutdown <- syscall.SIGTERM
+}
+
+// Handle sets a handler fucntion for a given HTTP method and path pair
+// to the application server mux.
+func (a *App) Handle(method string, group string, path string, handler Handler) {
+	h := func(w http.ResponseWriter, r *http.Request) {
+
+		// PRE CODE PROCESSING
+
+		if err := handler(r.Context(), w, r); err != nil {
+			// ERROR HANDLING
+			return
+		}
+
+		// POST CODE PROCESSING
+	}
+
+	finalPath := path
+	if group != "" {
+		finalPath = "/" + group + path
+	}
+
+	a.ContextMux.Handle(method, finalPath, h)
 }
