2810446a6d1fe52b734c14e632e46195c460ab46
Wojciech Wirkijowski <wojciech@wirkijowski.pl>
2023-04-20 08:36:07 +0200

    11.1 Maintaining Database Schemas

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
@@ -12,14 +13,45 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/wirkijowski/ultimate-go/business/data/schema"
+	"github.com/wirkijowski/ultimate-go/business/sys/database"
 )
 
 func main() {
-	err := genToken()
+	err := migrate()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func migrate() error {
+
+	cfg := database.Config{
+		User:         "postgres",
+		Password:     "postgres",
+		Host:         "0.0.0.0",
+		Name:         "postgres",
+		MaxIdleConns: 0,
+		MaxOpenConns: 0,
+		DisableTLS:   true,
+	}
+
+	db, err := database.Open(cfg)
+	if err != nil {
+		return fmt.Errorf("connect database: %w", err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := schema.Migrate(ctx, db); err != nil {
+		return fmt.Errorf("migrate database: %w", err)
+	}
+
+	fmt.Println("migrations complete")
+	return nil
 }
 
 func genToken() error {
